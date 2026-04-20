9098f9fa7cb0a404bfe62d0872ba0f3a298f17f1
Magnus128 <magu1000@stud.ek.dk>
2026-04-20 12:29:48 +0200

    3

--- src/main/java/UserMenu.java
+++ src/main/java/UserMenu.java
@@ -20,6 +20,16 @@ public class UserMenu implements Menu {
 				break;
 			case 2:
 				// All in specific category
+				System.out.println("""
+						1. Action	2. Adventure	3. Biography\s
+						4. Comedy	5. Crime	6. Drama\s
+						7. Family	8. Fantasy	9. Film-Noir\s
+						10. History	11. Horror	12. Music\s
+						13. Musical	14. Mystery	15. Romance\s
+						16. Sci-fi	17. Sport	18. Thriller\s
+						19. War	20. Western
+						""");
+				int categoryChoice = ui.promptNumeric("Choose a category: ");
 				break;
 			case 3:
 				// Previously watched
